9a9794dda115b0c77c6a513719015a0ff4301d1b
Yooyk13 <sykyagami@gmail.com>
2023-12-18 13:19:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
