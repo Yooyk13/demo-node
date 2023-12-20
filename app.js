8f440121f0b59441421a5ea6066262ee3d602b83
Yooyk13 <sykyagami@gmail.com>
2023-12-20 15:12:50 +0530

    Your commit message

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
